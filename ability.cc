9e3966a196f7caea3314f924aa3e71a392ed5a9d
Tony Wang <tonyy.wangg@gmail.com>
2019-11-27 02:00:09 -0500

    Added ability.h, ability.cc

---
+++ ability.cc
@@ -0,0 +1,5 @@
+#include "ability.h"
+
+void Ability::useAbility(){
+    isUsed = true; 
+}
--- ability.h
+++ ability.h
@@ -1,7 +1,14 @@
+#ifndef _ABILITY_H
+#define _ABILITY_H
+
 #include "enums.h"
 
 class Ability {
     private:
-        bool isUsed;
-        
+        bool isUsed = false;
+        AbilityType type; 
+    public:
+        void useAbility(); 
 };
+
+#endif
