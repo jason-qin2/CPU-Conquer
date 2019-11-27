8f6b904adf6388c8a709228c95e169a59bfa5d09
jasonator1228 <qinzechen@hotmail.com>
2019-11-27 01:10:08 -0500

    cell.h

--- cell.h
+++ cell.h
@@ -11,14 +11,14 @@ class Cell {
   size_t row; //row of cell
   size_t col; //column of cell
   Link *link; //link on cell, if any
-  std::vector<Ability> abilities; //abilities of cell
+  std::vector<Ability> *abilities; //abilities of cell
 
 public:
   Cell(size_t r, size_t c); //constructor
   bool hasLink(); //whether there is a link on the cell
-  Link getLink(); //returns link on the cell
+  Link *getLink(); //returns link on the cell
   bool hasAbilities(); //whether the cell has abilities
-  std::vector<Ability> getAbilities(); //gets the abilities of the cell
+  std::vector<Ability> *getAbilities(); //gets the abilities of the cell
   void setLink(Link link); //places a link on the cell
   void addAbility(Ability ability); //adds an ability to the cell
 };
