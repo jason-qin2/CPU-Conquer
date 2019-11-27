86b3249f7c03e0c148fe2ec7db63da28afd08088
Zechen Qin <z52qin@uwaterloo.ca>
2019-11-27 01:21:07 -0500

    Cell class

---
+++ cell.h
@@ -0,0 +1,26 @@
+#ifndef CELL_H
+#define CELL_H
+#include <vector>
+#include <cstddef>
+#include "enums.h"
+#include "link.h"
+#include "ability.h"
+
+class Cell {
+  bool hasServerPort; //whether the cell has a server port
+  size_t row; //row of cell
+  size_t col; //column of cell
+  Link *link; //link on cell, if any
+  std::vector<Ability*> abilities; //abilities of cell
+
+public:
+  Cell(size_t r, size_t c); //constructor
+  bool hasLink(); //whether there is a link on the cell
+  Link *getLink(); //returns link on the cell
+  bool hasAbilities(); //whether the cell has abilities
+  std::vector<Ability*> getAbilities(); //gets the abilities of the cell
+  void setLink(Link *link); //places a link on the cell
+  void addAbility(Ability *ability); //adds an ability to the cell
+};
+
+#endif
