a2573281dc57a193b78c88ec2fe02e3fe04056b5
Tony Wang <tony.wang1@uwaterloo.ca>
2019-11-27 01:13:31 -0500

    player.h, link.h

---
+++ link.h
@@ -0,0 +1,25 @@
+#ifndef _LINK_H
+#define _LINK_H
+
+#include <vector>
+#include "ability.h"
+#include "enums.h"
+#include "player.h"
+
+class Link { 
+    private:
+        std::vector<Ability*> Abilities;
+        int strength;
+        LinkType linkType;
+        char name; 
+        bool hidden = true;
+        Player ownedBy; 
+    public:
+        void changeType();
+        LinkType getLinkType();
+        int getStrength();
+        void show();
+        void addAbility(Ability *ability);
+}
+
+#endif
