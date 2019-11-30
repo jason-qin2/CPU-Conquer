7919bda4be12165acefc0a6d5b5efd7768bc9eac
Tony Wang <tonyy.wangg@gmail.com>
2019-11-29 23:21:31 -0500

    added to ability.h, ability.cc, and player.h

--- player.h
+++ player.h
@@ -3,6 +3,7 @@
 #include <vector> 
 #include "link.h"
 #include "enums.h"
+#include "grid.h"
 #include "ability.h"
 
 class Player {
@@ -13,10 +14,11 @@ class Player {
         std::vector<Ability*> abilities;
     public:
         void downloadLink(Link *link);
-        void useAbility(Ability *ability);
+        void useAbility(Ability *ability, Grid *theGrid);
         int getDlVirusCount();
         int getDlDataCount();
         int getAbilityCount();
+        std::vector<Link*> *getOwnedLinks();
 };
 
 #endif
