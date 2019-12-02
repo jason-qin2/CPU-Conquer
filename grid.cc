7c9d5ea888e6f96132b90a097ec9ea0239579eed
jasonator1228 <qinzechen@hotmail.com>
2019-12-02 02:06:26 -0500

    update grid

--- grid.cc
+++ grid.cc
@@ -1,35 +1,41 @@
+#include <vector>
+#include <iostream>
 #include "grid.h"
+#include "cell.h"
+
+
+using namespace std;
 
 bool Grid::isFinished() {
-    for (auto &player: players) {
-        if (player->downloadedDataCount() >= 4) {
-            return true;
-        } else if (player->downloadedVirusCount() >= 4) {
-            return true;
-        }
+  for (auto &player: players) {
+    if (player->downloadedDataCount() >= 4) {
+      return true;
+    } else if (player->downloadedVirusCount() >= 4) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 Player *Grid::whoWon() {
-    Player *playerOne = players[0];
-    Player *playerTwo = players[1];
-    if ((playerOne->downloadedDataCount() >= 4) ||
-        (playerTwo->downloadedVirusCount() >= 4)) {
-        return playerOne;
-    } else if ((playerTwo->downloadedDataCount() >= 4) ||
-        (playerOne->downloadedVirusCount() >= 4)) {
-        return playerTwo;
-    }
-    return nullptr;
+  Player *playerOne = players[0];
+  Player *playerTwo = players[1];
+  if ((playerOne->downloadedDataCount() >= 4) ||
+  (playerTwo->downloadedVirusCount() >= 4)) {
+    return playerOne;
+  } else if ((playerTwo->downloadedDataCount() >= 4) ||
+  (playerOne->downloadedVirusCount() >= 4)) {
+    return playerTwo;
+  }
+  return nullptr;
 }
 
 void Grid::init() {
-    for (size_t i = 0; i < defaultGridSize; i++) {
-        for (size_t j = 0; j < defaultGridSize; j++) {
-            theGrid[i].push_back(Cell(i, j));
-        }
+  for (size_t i = 0; i < defaultGridSize; i++) {
+    for (size_t j = 0; j < defaultGridSize; j++) {
+      theGrid[i].push_back(Cell(i, j));
     }
+  }
 }
 
 bool isFirewall(Cell cell) {
@@ -54,7 +60,7 @@ void firewall(Cell cell) {
   Link *link = cell.getLink();
   int playerNum;
   for (int i = 0; i < cell.getAbilities().size(); i++) {
-    if (cell.getAbilities()[i].getAbilityType() == AbilityType::Firewall) {
+    if (cell.getLink()->getAbilities()[i].getAbilityType() == AbilityType::Firewall) {
       playerNum = cell.getAbilities()[i].getPlayerNum();
     }
   }
@@ -69,16 +75,48 @@ void firewall(Cell cell) {
   }
 }
 
-bool isValidMove(size_t row, size_t col) {
-  if (activePlayer->getPlayerNumber() == 1) {
-    
-  }
-  else {
-
+bool isValidMove(size_t row, size_t col, Player *player) {
+  int playerNum = player->getPLayerNumber();
+  Cell newCell = theGrid[row][col];
+  if (row < 8 && col < 8) {
+    return true;
+  } else if (newCell.hasLink()) {
+    if (newCell.getLink()->getPlayerNum() == playerNum) {
+      return false;
+    }
+  } else if (newCell.hasServerPort()) {
+    return false;
   }
 }
 
-void moveLink(Link *link, Direction dir) {
+void moveOffGrid(size_t row, size_t col, Player *Player, Link *link) {
+  int playerNum = player->getPLayerNumber();
+  if (playerNum == 1) {
+    if (row > 7 && col >= 0 && col <= 7) {
+      player->downloadLink(link);
+    }
+  }
+  if (playerNum == 2) {
+    if (row < 0 && col >= 0 && col <= 7) {
+      player->downloadLink(link);
+    }
+  }
+}
+
+void move(Cell cell, size_t row, size_t col, Player *player, Link *link) {
+  if (isValidMove(row, col)) {
+    cell = theGrid[row][col];
+    if (isFirewall(cell)) {
+      firewall(cell);
+    }
+    cell.setLink(link);
+  }
+  else {
+    moveOffGrid(row, col, player, link);
+  }
+}
+
+void Link::moveLink(Link *link, Direction dir) {
   Cell cell;
   size_t row;
   size_t col;
@@ -93,52 +131,37 @@ void moveLink(Link *link, Direction dir) {
   }
   if (dir = Direction::North) {
     if (isLinkBoost(cell)) {
-      cell = theGrid[row - 2][col];
+      move(cell, row - 2, col, activePlayer, link);
     }
     else {
-      cell = theGrid[row - 1][col];
+      move(cell, row - 1, col, activePlayer, link);
     }
-    if (isFirewall(cell)) {
-      firewall(cell);
-    }
-    cell.setLink(link);
   }
   else if (dir = Direction::East) {
     if (isLinkBoost(cell)) {
-      cell = theGrid[row][col + 2];
+      move(cell, row, col + 2, activePlayer, link);
     }
     else {
-      cell = theGrid[row][col + 1];
+      move(cell, row, col + 1, activePlayer, link);
     }
-    if (isFirewall(cell)) {
-      firewall(cell);
-    }
-    cell.setLink(link);
   }
   else if (dir = Direction::South) {
     if (isLinkBoost(cell)) {
-      cell = theGrid[row + 2][col];
+      move(cell, row + 2, col, activePlayer, link);
     }
     else {
-      cell = theGrid[row + 1][col];
+      move(cell, row + 1, col, activePlayer, link);
     }
-    if (isFirewall(cell)) {
-      firewall(cell);
-    }
-    cell.setLink(link);
   }
   else if (dir = Direction::West) {
     if (isLinkBoost(cell)) {
-      cell = theGrid[row][col - 2];
+      move(cell, row, col - 2, activePlayer, link);
     }
     else {
-      cell = theGrid[row][col - 1];
+      move(cell, row, col - 1, activePlayer, link);
     }
-    if (isFirewall(cell)) {
-      firewall(cell);
-    }
-    cell.setLink(link);
   }
+
 } // moves a link a certain direction
 // void addGraphicsDisplay(GraphicsDisplay graphicsDisplay) // adds GraphicsDisplay
 void addTextDisplay(TextDisplay textDisplay); // adds TextDisplay
