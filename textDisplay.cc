de054bda0a4a40ee0b515d7fbddf2c17884dc4a0
jasonator1228 <qinzechen@hotmail.com>
2019-12-02 00:00:13 -0500

    textDisplay

--- textDisplay.cc
+++ textDisplay.cc
@@ -12,12 +12,39 @@ TextDisplay::TextDisplay() {
     theDisplay.push_back(row);
   }
 }
+
+bool isFirewall(Cell cell) {
+  for (int i = 0; i < cell.getAbilities().size(); i++) {
+    if (cell.getAbilities()[i].getAbilityType() == AbilityType::Firewall) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
+getPlayerNum();
 void TextDisplay::notify(Subject &whoFrom) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       Cell curCell = whoFrom[i][j];
       if (curCell.hasLink()) {
-        theDisplay[i][j] = curCell.getLink().getName();
+        if (isFirewall(curCell)) {
+          for (int i = 0; i < cell.getAbilities().size(); i++) {
+            if (cell.getAbilities()[i].getAbilityType() == AbilityType::Firewall) {
+              if (cell.getAbilities()[i].getPlayerNum() == 1) {
+                theDisplay[i][j] = 'm';
+              }
+              else if (cell.getAbilities()[i].getPlayerNum() == 2) {
+                theDisplay[i][j] = 'w';
+              }
+            }
+          }
+        }
+        else {
+          theDisplay[i][j] = curCell.getLink().getName();
+        }
       }
       else if (curCell.hasServerPort()) {
         theDisplay[i][j] = 'S';
