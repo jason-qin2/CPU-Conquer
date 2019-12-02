f761829be020e61de633797930280f81ac1e6814
Zechen Qin <z52qin@uwaterloo.ca>
2019-12-02 01:39:10 -0500

    Textdisplay

---
+++ textDisplay.cc
@@ -0,0 +1,62 @@
+#include <vector>
+#include "textDisplay.h"
+
+using namespace std;
+
+TextDisplay::TextDisplay() {
+  for (int i = 0; i < 8; i++) {
+    vector<char> row;
+    for (int j = 0; j < 8; j++) {
+      row.push_back('.');
+    }
+    theDisplay.push_back(row);
+  }
+}
+
+int getFirewallOwner(Cell cell) {
+  std::vector<Ability*> abilities = cell.getAbilities();
+  for (int i = 0; i < abilities.size(); i++) {
+    if (abilities[i].getAbilityType() == AbilityType::Firewall) {
+      if (abilities[i].getPlayerNum() == 1) {
+        return 1;
+      }
+      else if (abilities[i].getPlayerNum() == 2) {
+        return 2;
+      }
+    }
+  }
+  return 0;
+}
+
+void TextDisplay::notify(Subject &whoFrom) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      Cell curCell = whoFrom[i][j];
+      if (curCell.hasLink()) {
+        theDisplay[i][j] = curCell.getLink().getName();
+      }
+      else if (curCell.hasServerPort()) {
+        theDisplay[i][j] = 'S';
+      }
+      else if (getFirewallOwner(curCell) == 1) {
+        theDisplay[i][j] = 'm';
+      }
+      else if (getFirewallOwner(curCell) == 2) {
+        theDisplay[i][j] = 'w';
+      }
+      else {
+        theDisplay[i][j] = '.';
+      }
+    }
+  }
+}
+
+friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td) {
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      out << td.theDisplay[i][j];
+    }
+    out << endl;
+  }
+  return out;
+}
